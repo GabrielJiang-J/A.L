c8101e79c05b980175fa9247d86bff510bdb5901
Jiang Ziqi <jiangziqi@360.cn>
2021-05-19 14:45:24 +0800

    doc(sort): update unnested quick sort
    Signed-off-by: Jiang Ziqi <jiangziqi@360.cn>

--- c/project/sort/quick_sort/quick.c
+++ c/project/sort/quick_sort/quick.c
@@ -37,12 +37,45 @@ void quick_sort(int *nums, int left, int right)
     }
 }
 
+void quick_sort_unnest(int *nums, int left, int right)
+{
+    int stack[1024] = { 0 };
+    int rsp = -1;
+    int low = 0, high = 0;
+    int p = 0;
+
+    stack[++rsp] = right;
+    stack[++rsp] = left;
+
+    while (rsp != -1) {
+        low = stack[rsp--];
+        high = stack[rsp--];
+
+        if (low < high) {
+            p = quick_helper(nums, low, high);
+            if (p > low) {
+                stack[++rsp] = p - 1;
+                stack[++rsp] = low;
+            }
+
+            if (high > p) {
+                stack[++rsp] = high;
+                stack[++rsp] = p + 1;
+            }
+        }
+    }
+}
+
 int main(int argc, char *argv[])
 {
 #define SIZE 15
     int nums[SIZE] = {3, 4, 8, 10, 9, 1, 7, 2, 6, 5, 300, 100, 500, 1000, 600};
     
+#if 0
     quick_sort(nums, 0, SIZE - 1);
+#else
+    quick_sort_unnest(nums, 0, SIZE - 1);
+#endif
 
     for (int i = 0; i < SIZE; i++) {
         printf("%d ", nums[i]); 
